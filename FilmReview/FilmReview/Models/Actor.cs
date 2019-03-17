24129b093fd8e3603bf4e47ab7c2e895f8806375
Nicholas Randall <P17199825@my365.dmu.ac.uk>
2019-03-17 22:24:32 +0000

    Added Actor model

---
+++ FilmReview/FilmReview/Models/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmReview.Models
+{
+    public class Actor
+    {
+        public virtual int ActorID { get; set; }
+
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+
+        public virtual int FilmId { get; set; }
+
+        public virtual Film Film { get; set; }
+
+    }
+}
