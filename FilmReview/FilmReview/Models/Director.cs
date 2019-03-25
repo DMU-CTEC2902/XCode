508afce10c1369b8b59a6a180e3e129d5392e04b
NickR123 <Nickrandall5@gmail.com>
2019-03-25 12:03:36 +0000

    added Director

---
+++ FilmReview/FilmReview/Models/Director.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmReview.Models
+{
+    public class Director
+    {
+        [Key]
+        public virtual int DirectorID { get; set; }
+
+        public virtual string DirectorFirstName { get; set; }
+
+        public virtual string DirectorLastName { get; set; }
+
+        public virtual int ActorID { get; set; }
+
+        public virtual int FilmId { get; set; }
+
+        public virtual Actor Actor { get; set; }
+
+        public virtual Film Film { get; set; }
+    }
+}
